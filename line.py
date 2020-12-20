1a59fbe3cfef5c5e569de50edf03d8d59cfe0b05
Tsundere Chen <me@tsunderechen.io>
2020-12-21 03:50:39 +0800

    Add line.py as helper

---
+++ line.py
@@ -0,0 +1,24 @@
+from linebot import LineBotApi
+from linebot.models import TextSendMessage, ImageSendMessage
+from linebot.exceptions import LineBotApiError
+
+from config import channel_secret, channel_access_token
+
+lineBotApi = LineBotApi(channel_access_token)
+
+def webhook_parser(webhook):
+    signature = 
+    event = webhook["events"][0]
+    replyToken = event["replyToken"]
+    userId = event["source"]["userId"]
+    message = event["message"]["text"]
+
+    return replyToken, userId, message
+
+class LineAPI:
+    @staticmethod
+    def sendReplyMessage(replyToken, replyMessage):
+        try:
+            lineBotApi.reply_message(replyToken, TextSendMessage(replyMessage))
+        except LineBotApiError:
+            print(LineBotApiError)
