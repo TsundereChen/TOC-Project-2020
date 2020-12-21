8027878b895cd28c12285ab8663d9bcbfd815cc6
Tsundere Chen <me@tsunderechen.io>
2020-12-21 16:24:24 +0800

    Add function to check latest price

--- fsm.py
+++ fsm.py
@@ -1,7 +1,11 @@
 from transitions.extensions import GraphMachine
 
-from utils import send_text_message
+from utils import generateQueryString, queryValidChecker, priceParser
 
+from config import influxdb_host, influxdb_port, influxdb_username, influxdb_password
+from influxdb import InfluxDBClient
+
+client = InfluxDBClient(influxdb_host, influxdb_port, influxdb_username, influxdb_password)
 
 class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
@@ -59,13 +63,13 @@ class TocMachine(GraphMachine):
                             'trigger': 'goNewsOrGoBack',
                             'source': 'latestPrice',
                             'dest': 'news',
-                            'conditions': 'goNews'
+                            'conditions': 'choosedNews'
                         },
                         {
                             'trigger': 'goNewsorGoBack',
                             'source': 'latestPrice',
                             'dest': 'options',
-                            'conditions': 'goBackOptions',
+                            'conditions': 'choosedOptions',
                         },
                         {
                             'trigger': 'enterHistoricalPrice',
@@ -83,13 +87,13 @@ class TocMachine(GraphMachine):
                             'trigger': 'goNewsOrGoBack',
                             'source': 'historicalPrice',
                             'dest': 'news',
-                            'conditions': 'goNews'
+                            'conditions': 'choosedNews'
                         },
                         {
                             'trigger': 'goNewsOrGoBack',
                             'source': 'historicalPrice',
                             'dest': 'options',
-                            'conditions': 'goBackOptions'
+                            'conditions': 'choosedOptions'
                         },
                         {
                             'trigger': 'enterNews',
@@ -113,3 +117,55 @@ class TocMachine(GraphMachine):
                     "auto_transitions": False,
                 }
             )
+
+    def choosedLatest(self, replyToken, message):
+        return text == "1"
+    def choosedHistorical(self, replyToken, message):
+        return text == "2"
+    def choosedNews(self, replyToken, message):
+        return text == "3"
+    def choosedOptions(self, replyToken, message):
+        return text == "0"
+
+    def validCryptocurrency(self, replyToken, message):
+        if len(message) > 5:
+            return False
+        result = client.query(generateQueryString(message, "latest"))
+        return queryValidChecker(result)
+
+    def invalidCryptocurrency(self, replyToken, message):
+        if len(message) > 5:
+            return True
+        result = client.query(generateQueryString(message "latest"))
+        LineAPI.sendReplyMessage(replyToken, "You entered invalid cryptocurrency!")
+        return !queryValidChecker(result)
+
+    def on_enter_options(self, replyToken, message):
+        options_str = """
+        Welcome to use cryptocurrency price checker.
+
+        [Latest] For the latest price, please enter 1.
+        [Historical] For historical price, please enter 2.
+        [News] For related news, please enter 3.
+        """
+        LineAPI.sendReplyMessage(replyToken, options_str)
+
+    def on_enter_latestCheck(self, replyToken, message):
+        prompt_str = """
+        Please enter the cryptocurrency you want to query.
+        For example, if you want to check Bitcoin, enter BTC.
+        If you want to check Ethereum, enter ETH.
+        """
+        LineAPI.sendReplyMessage(replyToken, message)
+
+    def on_enter_latestPrice(self, replyToken, message):
+        result = client.query(generateQueryString(message, "latest"))
+        price = priceParser(result)
+        returnString = "The price of " + str(message) + " is " + str(price) + " USD now."
+        self.lastCheckCurrency = str(message)
+        LineAPI.sendReplyMessage(replyToken, returnString)
+        returnString = """
+        [News] If you want to check the news, enter 3
+        [Options] If you want to go back to options, enter 0
+        """
+        LineAPI.sendReplyMessage(replyToken, returnString)
